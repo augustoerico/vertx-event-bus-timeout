afdf3a47b28c3b5e59edb3d0ed7ca8b24c660767
Erico Silva <augusto.ericosilva@gmail.com>
2017-02-02 15:34:25 -0200

    Integrate with verticles

--- src/main/groovy/io/github/augustoerico/server/Application.groovy
+++ src/main/groovy/io/github/augustoerico/server/Application.groovy
@@ -1,5 +1,7 @@
 package io.github.augustoerico.server
 
+import io.github.augustoerico.api.Bye
+import io.github.augustoerico.api.Hello
 import io.vertx.groovy.core.Vertx
 import io.vertx.groovy.core.http.HttpServer
 import io.vertx.groovy.ext.web.Router
@@ -17,20 +19,17 @@ class Application {
             Router router = Router.router(vertx)
 
             router.route('/hello').handler({ RoutingContext context ->
-                // This handler gets called for each request that arrives on the server
                 def response = context.response()
                 response.putHeader('content-type', 'text/plain')
 
-                // Write to the response and end it
-                response.end('Hello!')
+                Hello.create(vertx).sayHello('Erico', { response.end('Say hello!') })
             })
 
             router.route('/bye').handler({ RoutingContext context ->
-
                 def response = context.response()
                 response.putHeader('content-type', 'text/plain')
 
-                response.end('Bye!')
+                Bye.create(vertx).sayBye('Erico', { response.end('Say bye!') })
             })
 
             server.requestHandler(router.&accept).listen(8080)
