3079da61518d638338df03c0e5cd37710aff5a29
Erico Silva <augusto.ericosilva@gmail.com>
2017-02-02 15:28:04 -0200

    Add routes

--- src/main/groovy/io/github/augustoerico/server/Application.groovy
+++ src/main/groovy/io/github/augustoerico/server/Application.groovy
@@ -1,7 +1,9 @@
 package io.github.augustoerico.server
 
-import io.vertx.core.Vertx
-import io.vertx.core.http.HttpServer
+import io.vertx.groovy.core.Vertx
+import io.vertx.groovy.core.http.HttpServer
+import io.vertx.groovy.ext.web.Router
+import io.vertx.groovy.ext.web.RoutingContext
 
 class Application {
 
@@ -12,16 +14,26 @@ class Application {
 
             HttpServer server = vertx.createHttpServer()
 
-            server.requestHandler({ request ->
+            Router router = Router.router(vertx)
+
+            router.route('/hello').handler({ RoutingContext context ->
                 // This handler gets called for each request that arrives on the server
-                def response = request.response()
+                def response = context.response()
                 response.putHeader('content-type', 'text/plain')
 
                 // Write to the response and end it
-                response.end('Hello World!')
+                response.end('Hello!')
             })
 
-            server.listen(8080)
+            router.route('/bye').handler({ RoutingContext context ->
+
+                def response = context.response()
+                response.putHeader('content-type', 'text/plain')
+
+                response.end('Bye!')
+            })
+
+            server.requestHandler(router.&accept).listen(8080)
         })
     }
 
